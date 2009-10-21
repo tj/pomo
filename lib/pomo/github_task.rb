b559c6a5e9506505bfd771bd835fbb34e7a74b9e
visionmedia <tj@vision-media.ca>
2009-10-21 15:37:59 -0700

    Added several metadata related methods to Pomo::GithubTask

--- lib/pomo/github_task.rb
+++ lib/pomo/github_task.rb
@@ -1,24 +1,53 @@
 
 module Pomo
+  class Task
+    def github?
+      false
+    end  
+  end
+  
   class GithubTask < Task
     
     ##
     # Username.
     
-    attr_reader :username
+    attr_accessor :username
     
     ##
     # Project name.
     
-    attr_reader :project
+    attr_accessor :project
+    
+    ##
+    # Labels array.
+    
+    attr_accessor :labels
+    
+    ##
+    # Issue number.
+    
+    attr_accessor :number
     
     ##
     # Initialize with _name_ and _options_.
     
     def initialize name = nil, options = {}
       super
-      @username = options.delete :username
-      @project = options.delete :project
+      options.each { |k,v| send :"#{k}=", v }
+    end
+    
+    ##
+    # Check if the task is a github issue.
+    
+    def github?
+      true
+    end
+    
+    ##
+    # Absolute URI to github issue.
+    
+    def uri
+      "http://github.com/#{username}/#{project}/issues#issue/#{number}"
     end
     
   end
